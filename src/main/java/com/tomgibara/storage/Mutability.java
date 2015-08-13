aa8f90f4b6afaba530b01d1e65800abf84f03738
Tom Gibara <me@tomgibara.com>
2015-08-13 22:48:32 +0100

    Documents mutability interface.

--- src/main/java/com/tomgibara/storage/Mutability.java
+++ src/main/java/com/tomgibara/storage/Mutability.java
@@ -1,22 +1,62 @@
 package com.tomgibara.storage;
 
+/**
+ * Exposes methods for manipulating mutability.
+ * 
+ * @author Tom Gibara
+ *
+ * @param <T> the underlying type
+ */
+
 public interface Mutability<T> {
 
-	// whether mutable
+	/**
+	 * Whether the object is mutable.
+	 * 
+	 * @return true if mutable, false otherwise
+	 */
 	boolean isMutable();
 	
-	// creates a mutable copy if not mutable
+	/**
+	 * A copy of the object if it is immutable, or the object itself. This
+	 * method is useful when receiving an object of uncertain mutability on
+	 * which mutation-based computations are necessary.
+	 * 
+	 * @return the mutable object or a mutable copy
+	 */
 	T mutable();
 	
-	// creates a mutable copy
+	/**
+	 * A mutable copy of the object. Changes to the state of the returned copy
+	 * will not modify this object.
+	 * 
+	 * @return a mutable copy of this object
+	 */
 	T mutableCopy();
 	
-	// creates a mutable view if not mutable
+	/**
+	 * An immutable copy of the object if it is mutable or the object itself.
+	 * This method is useful when returning an object of uncertain mutability
+	 * to prevent accidental mutation by the caller.
+	 * 
+	 * @return the immutable object or an immutable copy.
+	 */
 	T immutable();
 	
-	// creates an immutable copy
+	/**
+	 * An immutable copy of this object.
+	 * 
+	 * @return an immutable copy of this object
+	 */
 	T immutableCopy();
 	
-	// creates an immutable view
+	/**
+	 * An immutable view of this object. The view will exhibit the same state as
+	 * this object, and mutations of the state of this object will be reflected
+	 * in the view, but mutation via calls on the returned view will be
+	 * prohibited.
+	 * 
+	 * @return an immutable view of this object
+	 */
 	T immutableView();
 }
