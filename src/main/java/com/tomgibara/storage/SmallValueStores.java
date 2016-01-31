b1de8cd0ce0f5baa2856ed3fe16e46d47ba3a2ed
Tom Gibara <me@tomgibara.com>
2016-01-31 06:52:50 +0000

    Adds masking to SmallValueStores - possibly temporary.

--- src/main/java/com/tomgibara/storage/SmallValueStores.java
+++ src/main/java/com/tomgibara/storage/SmallValueStores.java
@@ -540,12 +540,14 @@ abstract class SmallValueStore extends AbstractStore<Integer> {
 
 		private final int range;
 		private final int count;
+		private final int mask;
 		private final BitStore bits;
 
 		ArbitraryStore(int size, int range) {
 			super(size);
 			this.range = range;
-			this.count = Integer.highestOneBit(range - 1) << 1;
+			count = Integer.highestOneBit(range - 1) << 1;
+			mask = (1 << count) - 1;
 			bits = Bits.store(size * count);
 		}
 
@@ -553,6 +555,7 @@ abstract class SmallValueStore extends AbstractStore<Integer> {
 			super(that.size());
 			this.range = that.range;
 			this.count = that.count;
+			this.mask = that.mask;
 			this.bits = bits;
 		}
 
@@ -596,14 +599,14 @@ abstract class SmallValueStore extends AbstractStore<Integer> {
 		@Override
 		int setInt(int index, int value) {
 			int position = index * count;
-			int v = (int) bits.getBits(position, count);
+			int v = ((int) bits.getBits(position, count)) & mask;
 			bits.setBits(position, value, count);
 			return v;
 		}
 		
 		@Override
 		int getInt(int index) {
-			return (int) bits.getBits(index * count, count);
+			return ((int) bits.getBits(index * count, count)) & mask;
 		}
 
 		@Override
