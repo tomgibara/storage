c500b7fc76d13772e19419ca631436cfcc2eecc7
Tom Gibara <me@tomgibara.com>
2016-10-26 22:55:54 +0100

    Documents StoreNullity.

--- src/main/java/com/tomgibara/storage/StoreNullity.java
+++ src/main/java/com/tomgibara/storage/StoreNullity.java
@@ -1,8 +1,32 @@
+/*
+ * Copyright 2016 Tom Gibara
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.tomgibara.storage;
 
 import java.util.Arrays;
 
 import com.tomgibara.fundament.Bijection;
+
+/**
+ * Instances of this class dictate how null values are treated by stores.
+ * 
+ * @author Tom Gibara
+ *
+ * @param <V> the type of any null-substituting value that applies
+ */
 
 public final class StoreNullity<V> {
 
@@ -30,16 +54,40 @@ public final class StoreNullity<V> {
 
 	// public statics
 
+	/**
+	 * A nullity that substitutes nulls with the specified value. The returned
+	 * nullity allows nulls to be set on a store ({@link #nullSettable()}
+	 * returns true) but will not allow nulls to be returned from it (
+	 * {@link #nullGettable()} returns false).
+	 * 
+	 * @param value
+	 *            the value to be substituted for null
+	 * @throws IllegalArgumentException
+	 *             if the substitute value is itself null
+	 * @return a nullity that substitutes nulls
+	 */
 	public static <V> StoreNullity<V> settingNullToValue(V value) {
 		if (value == null) throw new IllegalArgumentException("null value");
 		return new StoreNullity<>(value);
 	}
 
+	/**
+	 * A nullity that permits nulls to be stored and retrieved. Both
+	 * {@link #nullSettable()} and {@link #nullGettable()} return true.
+	 * 
+	 * @return a nullity that permits null values
+	 */
 	@SuppressWarnings("unchecked")
 	public static <V> StoreNullity<V> settingNullAllowed() {
 		return (StoreNullity<V>) nullAllowed;
 	}
 
+	/**
+	 * A nullity that does not permit nulls to be stored or retrieved. Both
+	 * {@link #nullSettable()} and {@link #nullGettable()} return false.
+	 * 
+	 * @return a nullity that prohibits null values
+	 */
 	@SuppressWarnings("unchecked")
 	public static <V> StoreNullity<V> settingNullDisallowed() {
 		return (StoreNullity<V>) nullDisallowed;
@@ -47,11 +95,13 @@ public final class StoreNullity<V> {
 
 	/**
 	 * <p>
-	 * Suggests a suitable null value for a specified type. This method may be
-	 * used to identify an appropriate null value for stores of common types.
+	 * Suggests a nullity that can substitute null for a given type. This method
+	 * may be used to identify an appropriate null value for stores of common
+	 * types.
 	 *
 	 * <p>
-	 * It returns optionals wrapping the following values for the types given.
+	 * It returns {@link #settingNullToValue(Object)} wrapping the following
+	 * values for the types given.
 	 *
 	 * <dl>
 	 * <dt>primitive numeric types
@@ -63,15 +113,15 @@ public final class StoreNullity<V> {
 	 * <dt>primitive wrapper types
 	 * <dd><em>as per primitive types</dd>
 	 * <dt>enumerations
-	 * <dd>the enum constant with ordinal 0
+	 * <dd>the enum constant with ordinal 0 (if it exists)
 	 * <dt><code>java.lang.String</code>
 	 * <dd>the empty string <code>""</code>
 	 * </dl>
 	 *
 	 * <p>
-	 * For all other types, the method returns an empty optional. Future
-	 * implementations may return non-empty optionals for a greater number of
-	 * types.
+	 * For all other types, the method returns {@link #settingNullAllowed()}.
+	 * Future implementations may return null-replacing instances for a greater
+	 * number of types.
 	 *
 	 * @param type
 	 *            a type of stored value
@@ -127,14 +177,51 @@ public final class StoreNullity<V> {
 
 	// access methods
 
+	/**
+	 * Whether null values may be set on on stores. Some stores can store null
+	 * values and some stores substitute nulls with a default value, but some
+	 * stores will raise an <code>IllegalArgumentException</code> if any attempt
+	 * is made to introduce a null value into the store. A false value returned
+	 * from this method indicates the latter.
+	 * 
+	 * @return true if null values may be set on the store, false if not
+	 * @see Store#set(int, Object)
+	 */
 	public boolean nullSettable() {
 		return this != nullDisallowed;
 	}
 
+	/**
+	 * Whether null values may be returned from the store. Some stores can store
+	 * null values and some stores substitute nulls with a default value and
+	 * other stores will raise an exception if any attempt is made to introduce
+	 * a null value into the store. A true value returned from this method
+	 * indicates the former.
+	 * 
+	 * @return true if it is possible to get null values from the store, false
+	 *         if not
+	 * @see Store#get(int)
+	 */
 	public boolean nullGettable() {
 		return this == nullAllowed;
 	}
 
+	/**
+	 * <p>
+	 * Returns the value passed to {@link #settingNullToValue(Object)}, or null.
+	 * This value will replace null in any operation that attempts to introduce
+	 * nulls into a store to which this nullity applies.
+	 * 
+	 * <p>
+	 * Note that any non-null value returned by this method will by substituted
+	 * for null in calls to {@link Store#set(int, Object)} and
+	 * {@link Store#clear()} but occurrences of the value in the store will not
+	 * be reported as null.
+	 * 
+	 * @return the value substituted for null, or null if no substitution is
+	 *         performed
+	 * @see #settingNullToValue(Object)
+	 */
 	public V nullValue() {
 		return value;
 	}
--- src/test/java/com/tomgibara/storage/StoreNullityTest.java
+++ src/test/java/com/tomgibara/storage/StoreNullityTest.java
@@ -1,6 +1,7 @@
 package com.tomgibara.storage;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -15,5 +16,16 @@ public class StoreNullityTest {
 	public void testEnumDefault() {
 		assertEquals(NonEmptyEnum.DOG, StoreNullity.defaultForType(NonEmptyEnum.class).nullValue());
 		assertTrue(StoreNullity.defaultForType(EmptyEnum.class).nullSettable());
+	}
+
+	@Test
+	public void testConstructors() {
+		assertTrue(StoreNullity.settingNullAllowed().nullSettable());
+		assertTrue(StoreNullity.settingNullAllowed().nullGettable());
+		assertFalse(StoreNullity.settingNullDisallowed().nullSettable());
+		assertFalse(StoreNullity.settingNullDisallowed().nullGettable());
+		assertTrue(StoreNullity.settingNullToValue("").nullSettable());
+		assertFalse(StoreNullity.settingNullToValue("").nullGettable());
+		
 	}
 }
